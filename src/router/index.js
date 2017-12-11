75adbd73bbaca04c1673e4e752e85f4c34c16148
wangchen <1634718783@qq.com>
2017-12-11 13:59:03 +0800

    商品详情路由

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import startPage from '@/components/login/startPage.vue'
 import signRegister from '@/components/login/signRegister.vue'
 import forgetPwd from '@/components/login/forgetPwd.vue'
 import shopping from '@/components/shopping/shopping.vue'
+import shopinfo from '@/components/shopping/shopinfo.vue'
 
 Vue.use(Router)
 
@@ -13,6 +14,7 @@ export default new Router({
     {path: '/login', component: startPage},
     {path: '/signRegister/:type', component: signRegister},
     {path: '/forgetPwd/:id', component: forgetPwd},
-    {path: '/shopping', component: shopping}
+    {path: '/shopping', component: shopping},
+    {path: '/shopinfo/:id', component: shopinfo}
   ]
 })
