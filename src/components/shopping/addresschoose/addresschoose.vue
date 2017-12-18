54bcece763a3f97ff767f9b1772c942ae51b3975
wangchen <1634718783@qq.com>
2017-12-18 20:11:34 +0800

    地址选择

---
+++ src/components/shopping/addresschoose/addresschoose.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="paytype">
+    <payheader></payheader>
+    <div class="type">
+    </div>
+    <div class="type">
+    </div>
+  </div>
+</template>
+<script>
+import payheader from './header.vue'
+export default {
+  components: {
+    payheader
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+<style scoped>
+.paytype{
+  min-height: 100vh;
+  background: #f1f1f1;
+}
+.type{
+  display: flex;
+  width: 100%;
+  padding: 4vw 0;
+  background: white;
+  border-bottom: 1px solid #dddddd;
+}
+</style>
---
+++ src/components/shopping/addresschoose/header.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="header">
+    <header>
+      <div class="left" @click="back()"><img src="../../../assets/images/shopping/order/back.jpg">返回</div>
+      <div class="right">
+        <img src="../../../assets/images/shopping/talk.png">
+      </div>
+      <h2>地址选择</h2>
+    </header>
+    <div style="height: 50px"></div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      status: false
+    }
+  },
+  methods: {
+    back () {
+      this.$router.push('/ordersure')
+    }
+  }
+}
+</script>
+<style scoped>
+/* 头部组件 */
+  header{
+    width: 100%;
+    z-index: 10;
+    background: white;
+    line-height: 50px;
+    height: 50px;
+    position: fixed;
+    top: 0;
+    box-sizing: border-box;
+    border-bottom: 1px solid #ca3232; 
+  }
+  header .right{
+    position: absolute;
+    right: 3%;
+  }
+  header .right img{
+    width: 26px;
+    height: 26px;
+    margin-top: -6px;
+    vertical-align: middle;
+    margin-left: 8px;
+  }
+  header h2{
+    font-size: 1.2rem; 
+    text-align: center;
+  }
+  .left{
+    position: absolute;
+    left: 0%;
+  }
+  .left img{
+    margin-right: 5px;
+    height: 30px;
+    vertical-align: -10px;
+  }
+  header .border{
+    display: inline-block;
+    line-height: 24px;
+    padding: 0 6px;
+    color: #bbbbbb;
+    border-radius: 6px; 
+    border: 1px solid #bbbbbb;
+  }
+  header .red{
+    color: red;
+    border: 1px solid red;    
+  }
+</style>
