4a767cfb9a6661ca202762ccbe738ccb331bd350
wangchen <1634718783@qq.com>
2017-12-18 20:12:28 +0800

    支付方式

---
+++ src/components/shopping/paytype/header.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="header">
+    <header>
+      <div class="left" @click="back()"><img src="../../../assets/images/shopping/order/back.jpg">返回</div>
+      <h2>选择支付方式</h2>
+    </header>
+    <div style="height: 50px"></div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      status: false
+    }
+  },
+  methods: {
+    back () {
+      this.$router.push('/ordersure')
+    }
+  }
+}
+</script>
+<style scoped>
+  header{
+    width: 100%;
+    z-index: 10;
+    background: white;
+    line-height: 50px;
+    height: 50px;
+    position: fixed;
+    top: 0;
+    box-sizing: border-box;
+    border-bottom: 1px solid #ca3232; 
+  }
+  header h2{
+    font-size: 1.2rem; 
+    text-align: center;
+  }
+  .left{
+    position: absolute;
+    left: 0%;
+  }
+  .left img{
+    margin-right: 5px;
+    height: 30px;
+    vertical-align: -10px;
+  }
+  header .border{
+    display: inline-block;
+    line-height: 24px;
+    padding: 0 6px;
+    color: #bbbbbb;
+    border-radius: 6px; 
+    border: 1px solid #bbbbbb;
+  }
+  header .red{
+    color: red;
+    border: 1px solid red;    
+  }
+</style>
---
+++ src/components/shopping/paytype/paytype.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="paytype">
+    <payheader></payheader>
+    <div class="type" @click="zfb()">
+      <div><img src="../../../assets/images/zfb.png"></div>
+      <div>支付宝支付</div>
+      <div><img src="../../../assets/images/shopping/order/go.jpg"></div>
+    </div>
+    <div class="type" @click="wechat()">
+      <div><img src="../../../assets/images/wechat.png"></div>
+      <div>微信支付</div>
+      <div><img src="../../../assets/images/shopping/order/go.jpg"></div>
+    </div>
+  </div>
+</template>
+<script>
+import payheader from './header.vue'
+export default {
+  components: {
+    payheader
+  },
+  data () {
+    return {}
+  },
+  methods: {
+    zfb () {
+      this.$router.push('/shopping/ordermanage')
+    },
+    wechat () {
+      this.$router.push('/shopping/ordermanage')
+    }
+  }
+}
+</script>
+<style scoped>
+.paytype{
+  min-height: 100vh;
+  background: #f1f1f1;
+}
+.type{
+  display: flex;
+  width: 100%;
+  padding: 4vw 0;
+  background: white;
+  border-bottom: 1px solid #dddddd;
+}
+.type>div{
+  height: 12vw;
+}
+.type>div:first-child{
+  width: 18vw;
+  color: grey;
+}
+.type>div:nth-child(2){
+  width: 72vw;
+  line-height: 12vw;
+  font-size: 1.2rem;
+}
+.type>div:last-child{
+  width: 10vw;
+  text-align: right;
+}
+.type>div:first-child img{
+  margin: 0 3vw;
+  vertical-align: -4px;
+  height: 12vw;
+}
+.type>div:last-child img{
+  margin: 3vw;
+  width: 4vw;
+  /* vertical-align: -4px; */
+  height: 7vw;
+}
+</style>
